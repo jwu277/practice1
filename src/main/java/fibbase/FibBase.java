b255f119bf1ae81148bf66d105363daecbdcc67b
Sathish Gopalakrishnan <sathish.gopalakrishnan@gmail.com>
2018-11-05 14:04:17 -0800

    init repo

---
+++ src/main/java/fibbase/FibBase.java
@@ -0,0 +1,19 @@
+package fibbase;
+
+public class FibBase {
+
+	/**
+	 * 
+	 * @param a
+	 *            > 0
+	 * @param b
+	 *            >= a
+	 * @return y(a) + y(a+1) + y(a+2) + ... + y(b-1) + y(b)
+	 */
+	public static long ySum(long a, long b) {
+		// TODO: Implement this method using the definitions
+		// for y(a) in the README document.
+		return 0;
+	}
+
+}
